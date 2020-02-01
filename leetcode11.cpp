e56fab693d7d7f6e702bda65096e41e592d0a541
Prakalp Tiwari <48635172+PrakalpTiwari137@users.noreply.github.com>
2020-02-01 14:02:11 +0530

    Leetcode problem #11 : Container with Most Water
    Tag : Array

---
+++ leetcode11.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxarea = 0;
+        int l = 0; 
+        int r = height.size()-1;
+        while(l < r) {
+            maxarea = max(maxarea, (r-l)*min(height[r],height[l]));
+            if(height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+};
