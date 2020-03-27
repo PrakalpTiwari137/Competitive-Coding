6488b85a72d0e833ea9f306ff85e5a40efdc5247
Prakalp Tiwari <48635172+PrakalpTiwari137@users.noreply.github.com>
2020-03-27 22:54:04 +0530

    Create leetcode122.cpp
    #Best Time to Buy and Sell Stock II

---
+++ leetcode122.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() <2)
+            return 0;
+        int back = 0, forward =1;
+        int profit = 0;
+        while(forward < prices.size()) {
+            if(prices[forward] < prices[back]){
+                back = forward;
+                forward = back+1;
+            }
+            else {
+                while(forward+1<prices.size() && prices[forward+1]>prices[forward])
+                    forward++;
+                if(prices[forward]>=prices[back]){
+                    profit = profit+prices[forward]-prices[back];
+                    back = forward+1;
+                    forward = back+1;
+                }    
+            }       
+        }
+        return profit;
+    }
+};
