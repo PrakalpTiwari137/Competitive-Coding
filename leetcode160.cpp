efca8780272981dd074d9ecf1f09e83a59d47c8d
Prakalp Tiwari <48635172+PrakalpTiwari137@users.noreply.github.com>
2020-03-29 21:24:48 +0530

    Leetcode problem #160: Intersection of Two Linked Lists

---
+++ leetcode160.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* p = headA;
+        ListNode* q = headB;
+        while(p != q){
+            if(p != NULL){
+                p = p->next;
+            }
+            else {
+                p = headB;
+            }
+            if(q != NULL) {
+                q = q->next;
+            }
+            else{
+                q = headA;
+            }
+        }
+        return p;
+    }
+};
